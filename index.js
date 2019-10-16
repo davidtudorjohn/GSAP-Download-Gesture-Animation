e6737352040fa88c24b6f7190351d51e3b7f235c
David T. John <56046811+David-T-John@users.noreply.github.com>
2019-10-16 19:16:22 -0400

    Change button style and text content on Event

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ downloadBtn = document.getElementById('downloadBtn');
 
 downloadBtn.addEventListener('click', () => {
     TweenMax.to(".fa.fa-download", 2, {y:"240px", opacity:0, overflow:"visible"});
+    downloadBtn.style.backgroundColor = "#bebebe";
+    downloadBtn.textContent = "DONE";
 });
